41b72e582b8b5472eba942575ebf8278ff271f76
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-25 01:37:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,15 +17,14 @@ class Student
     self.create(row[1], row[2])
   end
 
-  def self.find_by_name(name)
-    sql = <<-SQL
-      SELECT * FROM students WHERE name = ?
-    SQL
-
-    result = DB[:conn].execute(sql, name)[0]
-    binding.pry
-    Student.new(result[0], result[1], result[2])
-  end
+  # def self.find_by_name(name)
+  #   sql = <<-SQL
+  #     SELECT * FROM students WHERE name = ?
+  #   SQL
+  #
+  #   result = DB[:conn].execute(sql, name)[0]
+  #   Student.new(result[0], result[1], result[2])
+  # end
 
   def self.create_table
     sql = <<-SQL
