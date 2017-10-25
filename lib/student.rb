8fde03646a37fb9e7bc396c1be187e6aaf8a4874
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-25 00:48:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,44 +14,45 @@ class Student
   end
 
   def self.create_table
-      sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS students (
-          id INTEGER PRIMARY KEY,
-          name TEXT,
-          grade TEXT
-        )
-        SQL
-      DB[:conn].execute(sql)
-    end
-
-    def self.drop_table
-      sql = <<-SQL
-        DROP TABLE students
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
       SQL
-      DB[:conn].execute(sql)
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+
     end
+  end
 
-    def save
-      if self.id
-        self.update
-      else
-        sql = <<-SQL
-          INSERT INTO students (name, grade)
-          VALUES (?, ?)
-        SQL
+  def update
+    sql = <<-SQL
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
+    SQL
 
-        DB[:conn].execute(sql, self.name, self.grade)
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
-      end
-
-      def update
-        sql = <<-SQL
-          UPDATE students SET name = ?, grade = ? WHERE id = ?
-        SQL
-
-        DB[:conn].execute(sql, self.name, self.grade, self.id)
-      end
-    end
 
 end #class Student
